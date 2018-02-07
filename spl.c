7af9d7517106cde61be2f1c47d372ae68d3b8655
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-02-07 16:26:31 +0530

    Create spl.c

---
+++ spl.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char str[100];
+countSpecialChar;
+int counter;
+countDigits=countSpecialChar;
+printf("Enter a string: ");
+scanf("%c",str);
+for(counter=0;str[counter]!=NULL;counter++)
+{
+if( countSpecialChar++)
+}
+printf("\nDigits: %d \nSpecial Characters: %d",countSpecialChar);
+return 0;

5720eeb7fd222d3a1228ed990f6cb6f5f56dabbf
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-02-07 16:32:02 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char str[100];
+int countnumeric;
+int counter;
+countDigits=countnumeric;
+printf("Enter a string: ");
+scanf("%c",str);
+for(counter=0;numeric[counter]!=NULL;counter++)
+{
+if( countnumeric++)
+}
+printf("\nDigits: %d \ncountnumeric: %d",countnumeric);
+return 0;
